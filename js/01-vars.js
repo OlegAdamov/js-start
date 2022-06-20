4b2aed1f40fe22698cc1edbf6d035d4c05ce3940
Oleg Adamov <olegadamov84@gmail.com>
2022-06-20 19:28:34 +0300

    0012

---
+++ js/01-vars.js
@@ -0,0 +1,20 @@
+
+
+const age = 10;
+const totalPrice = 200.74;
+const userName = "Chelsy";
+const message = "Добро пожаловать!";
+const isOpen = true;
+const shouldConfigm = false;
+
+
+console.log(totalPrice);
+
+const type = typeof userName;
+
+console.log(type);
+
+
+console.log("До");
+alert("messages");
+console.log("после");
---
+++ js/02-input.js
@@ -0,0 +1,9 @@
+// const shouldRenew = confirm("Хотите продлить подписку?");
+// console.log(shouldRenew);
+
+
+// let quantity = prompt("Enter number");
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
