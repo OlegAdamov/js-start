4e8dec0dd1422d86e4c8515346916e327f4a64c0
Oleg Adamov <olegadamov84@gmail.com>
2022-06-23 18:47:30 +0300

    23.06-18.47

---
+++ js/00-conspect.js
@@ -0,0 +1,142 @@
+"use strict";
+
+
+// const x1 = 10;
+// const x2 = 30;
+// const number = 15;
+
+// console.log(`Число ${number} попадает в отрезок до ${x1}? `, number < x1);
+
+// console.log(`Число ${number} попадает в отрезок после ${x2}? `, number > x2);
+
+// const res1 = number > x1 && number < x2;
+// console.log(`Число ${number} попадает в отрезок между ${x1} и ${x2}? `, res1);
+
+
+// const res2 = number < x1 || number > x2;
+// console.log(`Число ${number} попадает в отрезок до ${x1} или после ${x2}? `, res2);
+
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnd = false;
+
+// const canOpenChat = isOnline && isFriend && !isDnd;
+// console.log(`Можно открыть чать? `, canOpenChat);
+
+
+
+// if (number < x2) {
+//     console.log('stage1');
+// } else if (number > x2) {
+//     console.log('stage2');
+// } else {
+//     console.log('stage3');
+// }
+
+
+// const balance = -1000;
+
+// const message = balance >= 0 ? 'Позитивный баланс' : 'Негативный баланс';
+// console.log(message);
+
+// let cost;
+// const subscription = "free";
+
+// switch (subscription) {
+//     case "free":
+//         cost = 0;
+//         break;
+//     case "pro":
+//         cost = 100
+//         break;
+//     case "premium":
+//         cost = 500;
+//         break;
+//     default:
+//         console.log("Invalid subscription type");
+// }
+// console.log(cost);
+
+// const stars = 4
+//     ;
+// let price;
+
+// if (stars === 1 || stars === 2) {
+//     price = 20;
+// } else if (stars === 3 || stars === 4) {
+//     price = 30;
+// } else if (stars === 5) {
+//     price = 120
+// } else {
+//     console.log('Такого кол-ва звезд нет');
+// }
+
+// console.log(price);
+
+// switch (stars) {
+//     case 1:
+//     case 2:
+//         price = 20;
+//         break;
+//     case 3:
+//     case 4:
+//         price = 30;
+//         break;
+//     case 5:
+//         price = 120;
+//         break;
+    
+//     default:
+//         console.log('Такого кол-ва звезд нет');
+// }
+
+// console.log(price);
+
+
+// 1. Сделать переменные
+// const option = 3;
+// let message = 'ds';
+
+// 2. Сделать switch 1 2 3
+
+// switch(option) {
+//     case 1:
+//         message = 'Вы можете забрать товар завтра с 12:00 в нашем офисе';
+//         break;
+//     case 2:
+//         message = 'Курьер доставит заказ завтра с 9:00 до 18:00';
+//         break;
+//     case 3:
+//         message = 'Посылка будет отправлена сегодня';
+//         break;
+//     default:
+//         message = 'Вам перезвонит менеджер';
+
+// }
+// console.log(message);
+
+// 3. в каждом кейсе записать в message строку
+
+// 4. сделать лог message
+
+
+
+// FOR
+
+// for (let i = 5; i < 25; i += 2) {
+//     console.log(i);
+// }
+
+// console.log('Hello')
+
+
+
+
+
+
+
+
+
+
+
