22ec5e6537f5577f2f6e39b80382912a697e22b0
Oleg Adamov <olegadamov84@gmail.com>
2022-07-01 19:51:37 +0300

    01.07-19.51

--- js/00-test.js
+++ js/00-test.js
@@ -337,8 +337,15 @@ const findLogin = function (allLogins, loginToFind) {
 * при условии, что числа уникальные (не повторяются).
 */
 
-    
+    const numbers = [51, 18, 13, 24, 7, 85, 19];
+    let smallestNumber = numbers[0];
 
+    for (const number of numbers) {
+        if (number < smallestNumber) {
+            smallestNumber = number;
+        }
+    }
+    console.log(`smallestNumber: `, smallestNumber);
 
 
 
