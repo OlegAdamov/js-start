286da8c2895043b9e078af6c2a89a4166fc87253
Oleg Adamov <olegadamov84@gmail.com>
2022-07-04 17:09:14 +0300

    04.07-17.09

---
+++ js/conspect/Mod3-Lesson5.js
@@ -0,0 +1,90 @@
+console.log('Repeta: ');
+
+/* Объекты (делаем плейлист музыки: имя, рейтинг, треки, кол-во треков)
+* - Литерал объекта
+* - Свойства, ключи (имя) и значения
+* - Как отличить объект от области видимости
+*
+*
+*/
+
+
+// const playlist = {
+//     name: 'Мой супер плейлист',
+//     rating: 5,
+//     tracks: ['track-1', 'track-2', 'track-3'],
+//     trackCount: 3,
+// };
+
+// console.log(playlist);
+
+/* Доступ к свойству
+* - obj.key
+* - obj[key]
+* - Отсутствующие свойства
+*
+*/
+// const playlist = {
+//     name: 'Мой супер плейлист',
+//     rating: 5,
+//     tracks: ['track-1', 'track-2', 'track-3'],
+//     trackCount: 3,
+// };
+
+// console.log(playlist);
+// console.log(playlist.tracks);
+// console.log(playlist.name);
+// console.log(playlist.trackCount);
+
+// console.log(playlist['rating']);
+
+/* 
+* Короткая запись свойства
+*/
+const username = 'Mango';
+const email = 'mango@mail.com'
+
+const signupData = {
+    username: username,
+    email,
+};
+console.log(signupData)
+
+/* 
+* Вычисляемые свойства
+*/
+
+// <input name="color" value="tomato" >
+
+const inputName = 'color';
+const inputValue = 'tomato';
+
+const colorPickerData = {
+[inputName]: inputValue,
+};
+
+console.log(colorPickerData);
+
+/* 
+* 
+*
+*
+* - 
+* - 
+* - 
+*
+*/
+
+/* 
+* 
+*
+*
+* - 
+* - 
+* - 
+*
+*/
+
+
+
+
