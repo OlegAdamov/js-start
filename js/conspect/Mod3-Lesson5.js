165f18eeaddbc180a7e19a901d5f8a9947883cd4
Oleg Adamov <olegadamov84@gmail.com>
2022-07-07 14:40:10 +0300

    07.07-14.40

--- js/conspect/Mod3-Lesson5.js
+++ js/conspect/Mod3-Lesson5.js
@@ -191,16 +191,135 @@ console.log('Repeta: ');
 //     console.log('totalFeedback: ', totalFeedback);
 }
 
-/* 
-* 
-*
-*
-* - 
-* - 
-* - 
+{  console.log('Task - Работа с коллекцией(масивом объекта)');
+
+/*
 *
 */
 
+    const friends = [
+        { name: 'Mango', online: false },
+        { name: 'Kiwi', online: true },
+        { name: 'Poly', online: true },
+        { name: 'Ajax', online: false },
+    ];
+
+    // console.table(friends);
+
+    // for (const friend of friends) {
+    //     console.log(friend.name);
+
+    //     friend.newprop = 555;
+    // }
+    
+    console.table(friends);
+    /* 
+    * Ищем друга по имени
+    */
+    
+    const findFriendByName = function (allFriends, friendName) { 
+    for (const friend of allFriends) {
+        // console.log(friend);
+        // console.log(friend.name);
+
+        if (friend.name === friendName) {
+            return "COMPLETE!!!"
+        }
+        } 
+        return 'Do not complete !!!'
+};
+    // console.log(findFriendByName(friends, 'Poly'));
+    // console.log(findFriendByName(friends, 'Chelsy'));
+
+/* 
+* Получаем имена всех друзей
+*/
+    
+    const getAllNames = function (allFriends) {
+        const names = [];
+
+        for (const friend of allFriends) {
+            // console.log(friend.name);
+
+            names.push(friend.name);
+        }
+
+        return names;
+    };
 
 
+    // console.log(getAllNames(friends));
+
+    /*
+    * Получаем имена только друзей, которые онлайна
+    */
+    const getOnlineFriends = function (allFriends) {
+        const onlineFriends = [];
+
+        for (const friend of allFriends) {
+            // console.log(friend);
+            // console.log(friend.online);
+
+            if (friend.online) {
+                onlineFriends.push(friend);
+
+            }
+        }
+        return onlineFriends;
+    };
+    
+    // console.log(getOnlineFriends(friends));
+
+    const getOfflineFriends = function (allFriends) { 
+
+    // console.log(getOfLineFriends(friends));
+const offlineFriends = [];
+
+        for (const friend of allFriends) {
+            // console.log(friend);
+            // console.log(friend.online);
+
+            if (!friend.online) {
+                
+                offlineFriends.push(friend);
+            }
+        }
+        return offlineFriends;
+    
+    };
+    // console.log(getOfflineFriends(friends));
+
+    const getFriendsByOnlineStatus = function (allFriends) {
+        const friendsByStatus = {
+            online: [],
+            offline: [],
+        };
+
+        for (const friend of allFriends) {
+            if (friend.online) {
+                friendsByStatus.online.push(friend);
+                continue;
+            }
+
+            friendsByStatus.offline.push(friend);
+        }
+            
+        return friendsByStatus;
+    };
+
+        
+
+console.log(getFriendsByOnlineStatus(friends));
+}
+
+
+
+
+
+    
+    
+    
+/* 
+*
+*/
 
