362fde39f7ff7e0ff481b6362a710c4625b7ab1e
Oleg Adamov <olegadamov84@gmail.com>
2022-07-13 22:14:12 +0300

    13.07-22.14

--- js/conspect/Mod4-L7.js
+++ js/conspect/Mod4-L7.js
@@ -2,7 +2,7 @@
 
     
     const makeDish = function (sheffName, dish) {
-        console.log('${sheffName} готовит ${dish}');
+        console.log(`${sheffName} готовит ${dish}`);
     };
 
 
@@ -10,13 +10,13 @@
     makeDish(`Mango`, `омлет`);
     makeDish(`Mango`, `чай`);
     
-    makeDish('Poly', 'котлеты');
-    makeDish('Poly', 'супик');
-    makeDish('Poly', 'кофе');
+    makeDish(`Poly`, `котлеты`);
+    makeDish(`Poly`, `супик`);
+    makeDish(`Poly`, `кофе`);
 
     const makeSheff = function (name) {
         const makeDish = function (dish) {
-            console.log('${sheffName} готовит ${dish}');
+            console.log(`${sheffName} готовит ${dish}`);
         };
 
         return makeDish;
