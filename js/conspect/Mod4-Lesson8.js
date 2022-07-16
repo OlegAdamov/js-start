74ed1f2e911a31b1e31eac627c591ded417a6331
Oleg Adamov <olegadamov84@gmail.com>
2022-07-16 22:21:52 +0300

    16.07-20.21

---
+++ js/conspect/Mod4-Lesson8.js
@@ -0,0 +1,103 @@
+
+
+    // <script src="./js/conspect/" type="module"></script>
+
+    console.log('Repeta: ');
+
+
+    {console.log('Example: ');
+
+
+
+}
+
+    {console.log('Example: ');
+
+
+
+}
+
+    {console.log('Example: ');
+
+
+
+}
+
+    {console.log('Example: ');
+
+
+
+}
+
+
+
+{console.log('Autocheck: ');
+
+
+
+
+}
+
+{console.log('Autocheck: ');
+
+
+
+
+}
+
+{console.log('Autocheck: ');
+
+
+    
+
+}
+
+
+
+{console.log('Task-Konsp: ');
+
+    const planets = ["Земля", "Марс", "Венера", "Юпитер"];
+
+const planetsInUpperCase = planets.map(planet => planet.toUpperCase());
+console.log(planetsInUpperCase); // ['ЗЕМЛЯ', 'МАРС', 'ВЕНЕРА', 'ЮПИТЕР']
+
+const planetsInLowerCase = planets.map(planet => planet.toLowerCase());
+console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпитер']
+
+const planetsCase = planets.map(planet);
+console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпитер']
+
+// Оригинальный массив не изменился
+console.log(planets); // ['Земля', 'Марс', 'Венера', 'Юпитер']
+
+
+
+
+}
+
+{console.log('Task-Konsp: ');
+
+
+
+
+}
+
+{console.log('Task-Konsp: ');
+
+
+
+
+}
+
+
+
+{console.log(': ');
+
+    
+    
+    
+    
+    
+}
+
+
