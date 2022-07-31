5a9268f1d231a5ce143c078d03491b20837a4d5b
Oleg Adamov <olegadamov84@gmail.com>
2022-07-31 23:41:13 +0300

    31.07-23.41

--- js/conspect/Mod6-Lesson11.js
+++ js/conspect/Mod6-Lesson11.js
@@ -78,38 +78,53 @@
 {    console.log('Task-Konsp: DOM');
 
     
-{    console.log(document);
+// {    console.log(document);
 
-const body = document.body;
-console.log(body);
+// const body = document.body;
+// console.log(body);
 
-const list = body.firstElementChild;
-console.log(list);
+// const list = body.firstElementChild;
+// console.log(list);
 
-const firstListItem = list.firstElementChild;
-console.log(firstListItem);
+// const firstListItem = list.firstElementChild;
+// console.log(firstListItem);
 
-const listItems = list.children;
-    console.log(listItems);}
+// const listItems = list.children;
+//     console.log(listItems);}
 
+//     {
+//         const listWithId = document.querySelector('#menu');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log(listWithId);
+
+// const listWithClass = document.querySelector('.menu');
+// console.log(listWithClass);
+
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
+
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log(menuItemsByClass);
+
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = 'tomato';
+// console.log(firstMenuItem);
+//     }
+    
     {
-        const listWithId = document.querySelector('#menu');
-listWithId.style.textTransform = 'uppercase';
-listWithId.style.fontSize = '24px';
-console.log(listWithId);
+        //===================================================
+const message = document.querySelector("#message");
+console.log(message.value); // Default textarea message
 
-const listWithClass = document.querySelector('.menu');
-console.log(listWithClass);
+//===================================================
+const activeLink = document.querySelector(".btn.active");
+console.log(activeLink.href); // https://s.codepen.io/about
 
-const menuItemsByTagName = document.querySelectorAll("li");
-console.log(menuItemsByTagName);
-
-const menuItemsByClass = document.querySelectorAll(".menu-item");
-console.log(menuItemsByClass);
-
-const firstMenuItem = document.querySelector(".menu-item");
-firstMenuItem.style.color = 'tomato';
-console.log(firstMenuItem);
+//===================================================
+const image = document.querySelector(".image");
+console.log(image.src); // https://placeimg.com/640/480/animals
+image.src = 'https://placeimg.com/640/480/tech';
     }
     
 
