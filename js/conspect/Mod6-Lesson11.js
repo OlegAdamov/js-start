fa75f056cbeea7d73696c800069434de2e8b9493
Oleg Adamov <olegadamov84@gmail.com>
2022-08-02 14:29:38 +0300

    02.08-14.29

--- js/conspect/Mod6-Lesson11.js
+++ js/conspect/Mod6-Lesson11.js
@@ -38,19 +38,19 @@
         * https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480
         */
 
-        // magicBtn.addEventListener('click', () => {
-            const imageEl = document.querySelector('.hero__image');
-            // console.log('imageEl: ', imageEl);
-            // console.log(imageEl.src);
-            imageEl.src = 'https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480'
-            imageEl.alt = 'Это новый котик'
+        // // magicBtn.addEventListener('click', () => {
+        //     const imageEl = document.querySelector('.hero__image');
+        //     // console.log('imageEl: ', imageEl);
+        //     // console.log(imageEl.src);
+        //     imageEl.src = 'https://images.pexels.com/photos/1870376/pexels-photo-1870376.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=480'
+        //     imageEl.alt = 'Это новый котик'
        
 
-            const heroTitleEl = document.querySelector('.hero__title');
-            // console.log('heroTitleEl: ', heroTitleEl);
-            // console.log(heroTitleEl.textContent);
-            heroTitleEl.textContent = 'Я сладкий пирожочек!';
-        // });
+        //     const heroTitleEl = document.querySelector('.hero__title');
+        //     // console.log('heroTitleEl: ', heroTitleEl);
+        //     // console.log(heroTitleEl.textContent);
+        //     heroTitleEl.textContent = 'Я сладкий пирожочек!';
+        // // });
     
         console.log('Атрибуты')
             
@@ -127,12 +127,136 @@
 * https://fecore.net.ua/books/m5ph3r-javascript/module-o7/dom-traversal.html
 */
 
-const navEl = document.querySelector('.site-nav')
+// const navEl = document.querySelector('.site-nav')
 
-// const firstNavItemEl = navEl.querySelector('.site-nav__item')
-    const firstNavItemEl = navEl.firstElementChild;
-    console.log(firstNavItemEl);
-console.log(navEl.children)
+// // const firstNavItemEl = navEl.querySelector('.site-nav__item')
+//     const firstNavItemEl = navEl.firstElementChild;
+//     console.log(firstNavItemEl);
+// console.log(navEl.children)
+
+
+}
+
+{ console.log('Repeta: Создание элементов');
+
+/*
+* - Вставка узлов: appendChild(elem), insertBefore(elem, nextSibling),
+*   append(...elems), prepend(...elems)
+* - 
+*/
+
+
+/* 
+* Создаем заголовок
+*/
+    
+//    const titleEl = document.createElement('h1') 
+//     titleEl.classList.add('page-titile')
+//     titleEl.textContent = 'Это заголовок страницы :)'
+//     console.log(titleEl);
+    
+    // document.body.appendChild(titleEl);
+
+    
+/* 
+* Создаем изображение
+* https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988_1280.jpg
+* valais-alpine-mountains-glacier
+*/
+
+    // const imageEl = document.createElement('img');
+    // imageEl.src='https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988_1280.jpg'
+    // imageEl.alt='valais-alpine-mountains-glacier'
+    // imageEl.width = 640; // imageEl.setAttribute('width', 640);
+    // console.log('imageEl: ', imageEl);
+
+    // document.body.appendChild(imageEl);
+    
+    // const heroEl = document.querySelector('.hero');
+    // // heroEl.appendChild(titleEl);
+    // // heroEl.appendChild(imageEl);
+    // heroEl.append(titleEl, imageEl);
+
+
+/* 
+* Создаем и добавляем новый пункт меню
+*/
+    // const navItemEl = document.createElement('li');
+    // navItemEl.classList.add('site-nav__item');
+
+    // const navLinkEl = document.createElement('a');
+    // navLinkEl.href = '/profile';
+    // navLinkEl.classList.add('site-nav__link');
+    // navLinkEl.textContent = 'Личный кабинет';
+
+    // navItemEl.appendChild(navLinkEl);
+    // console.log(navItemEl)
+
+    // const navEl = document.querySelector('.site-nav');
+
+    // // navEl.appendChild(navItemEl)
+    // navEl.insertBefore(navItemEl, navEl.firstElementChild);
+
+
+
+}
+
+{
+    console.log('Repeta: Создание коллекции');
+
+    const colorPickerOptions = [
+        { label: 'red', color: '#F44336' },
+        { label: 'green', color: '#4CAF50' },
+        { label: 'blue', color: '#2196F3' },
+        { label: 'grey', color: '#607D8B' },
+        { label: 'pink', color: '#E91E63' },
+        { label: 'indigo', color: '#3F51B5' },
+    ];
+{
+    
+    //     const colorPickerContainerEl = document.querySelector('.js-color-picker');
+
+    //     const elements = [];
+
+    //     for (let i = 0; i < colorPickerOptions.length; i += 1) {
+    //         const option = colorPickerOptions[i];
+
+    //         const buttonEl = document.createElement('button');
+    // buttonEl.type = 'button';
+    // buttonEl.textContent = option.label;
+    // buttonEl.classList.add('color-picker__option')
+    // buttonEl.style.backgroundColor = option.color;
+            
+    //         elements.push(buttonEl);
+            
+    //     }
+    //     console.log(elements);
+    //     colorPickerContainerEl.append(...elements);
+}
+    
+  { 
+       const colorPickerContainerEl = document.querySelector('.js-color-picker');
+
+        const elements = colorPickerOptions.map(option => {
+   
+            const buttonEl = document.createElement('button');
+    buttonEl.type = 'button';
+    buttonEl.classList.add('color-picker__option')
+    buttonEl.textContent = option.label;
+    buttonEl.style.backgroundColor = option.color;
+
+         return buttonEl
+        });
+    console.log(elements);
+    
+        colorPickerContainerEl.append(...elements);
+}
+    
+
+ 
+    /* 
+    * Пишем функцию для создания разметки колорпикера
+    */
 
 
 }
