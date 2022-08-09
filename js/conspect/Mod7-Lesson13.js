c05df2efea2f3c52165086ed2130d89d546f465d
Oleg Adamov <olegadamov84@gmail.com>
2022-08-09 14:44:22 +0300

    09.08-14.44

--- js/conspect/Mod7-Lesson13.js
+++ js/conspect/Mod7-Lesson13.js
@@ -1,6 +1,46 @@
     console.log('Repeta: ');
 
-{ console.log('Repeta: ');
+{ console.log('Repeta: Делегирование событий');
+
+    /* 
+    * - общий слушатель
+    * - фильтр цели клика
+    */
+    
+    const container = document.querySelector('.js-container');
+
+    container.addEventListener('click', onClick);
+
+    function onClick(event) {
+        if (event.target.nodeName !== 'BUTTON') {
+            return;
+        }
+            console.log(event.target);
+            console.log(event.target.textContent);
+            console.log(event.target.nodeName);
+        
+    }
+
+    /* 
+    * Код добавления кнопок
+    */
+    const addBtn = document.querySelector('.js-add-btn');
+    let labelCounter = 6;
+
+    addBtn.addEventListener('click', onAddBtnClick);
+
+    function onAddBtnClick() {
+        const btn = document.createElement('button');
+        btn.textContent = `Кнопка ${labelCounter}`;
+        btn.type = 'button';
+
+        container.appendChild(btn);
+        labelCounter += 1;
+    }
+
+
+
+
 
 }
 
