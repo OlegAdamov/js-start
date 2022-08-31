8a76095f35034dd89d9db1310331523c7193348d
Oleg Adamov <olegadamov84@gmail.com>
2022-08-31 12:01:24 +0300

    pokemon

--- js/conspect/Mod10-Lesson19.js
+++ js/conspect/Mod10-Lesson19.js
@@ -1,12 +1,23 @@
     console.log('Repeta: ');
 
 { console.log('Repeta: ');
+    import pokemonCardTpl from '../templates/pokemon-card.hbs';
+
+    const refs = {
+        cardContainer: document.querySelector('.js-card-container'),
+    }
+
+
+
     fetch('https://pokeapi.co/api/v2/pokemon')
         .then(response => {
             return response.json();
         })
         .then(pokemon => {
             console.log(pokemon);
+            const markup = pokemonCardTpl(pokemon);
+            console.log(markup)
+            refs.cardContainer.innerHTML = markup
         })
         .catch(error => {
             console.log(error);
