cd56aef2d77b9ee48acd20377db38937bac5e61c
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2023-09-12 15:21:13 +0300

    train

--- js/conspect/Mod2-Lesson3.js
+++ js/conspect/Mod2-Lesson3.js
@@ -346,8 +346,21 @@
     // console.table(cards);
 }
 
+{
 
+    const arr = ['hello', 'world', '!'];
 
+    console.log(arr);
+
+    const arr1 = [1, 2, 3];
+    const arr2 = [4, 5, 6];
+    const arr3 = [7, 8];
+
+    const result = arr.concat(arr1, arr2, arr3);
+    console.log(arr);
+    console.log(result);
+
+}
 
 
 
