7a9431cd8fb954fb34da95317c14a8d19057f6ec
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2023-09-27 12:05:25 +0300

    Update Mod3-Lesson6.js

--- js/conspect/Mod3-Lesson6.js
+++ js/conspect/Mod3-Lesson6.js
@@ -633,10 +633,34 @@ console.log('Artem Rysich: ');
 
 }
 {    console.log(`Example 5 - Операція Spread`);
-/* 
-*
+/* Доповни функцію createContact(partialContact) так,
+* щоб вона повертала новий об'єкт контакту з доданими властивостями id та createdAt,
+* а також List зі значенням "default", якщо в partialContact немає такої властивості.
 */
+  
 
+  function createContact(partialContact) {
+    return {}
+  }
+
+  console.log(
+    createContact({
+      name: 'Mango',
+      email: 'mango@mail.com',
+      list: 'friends',
+    }),
+  );
+
+  console.log(
+    createContact({
+      name: 'Poly',
+      email: 'poly@hotmail.com',
+    }),
+  );
+
+  function createdId() {
+    return '_' + Math.random().toString(36).substr(29);
+  }
 
 }
 {    console.log(`Example - `);
