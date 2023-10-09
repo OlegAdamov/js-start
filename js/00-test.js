cc0125eee31d22028fc4fca100b1421d59137365
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2023-10-09 17:51:24 +0300

    Update 00-test.js

--- js/00-test.js
+++ js/00-test.js
@@ -968,7 +968,22 @@ function addBinary(a,b) {
     // console.log(friend(["Love", "Your", "Face", "1"])) // ["Love", "Your", "Face"]
 }
 
-{ console.log("%cMath operations", "color: green")
+{console.log("%cMath operations", "color: green")
+ 
+/* Your task is to create a function that does four basic mathematical operations.
+*
+* The function should take three arguments - operation(string/char), value1(number), value2(number).
+* The function should return result of numbers after applying the chosen operation.
+* 
+* Examples(Operator, value1, value2) --> output
+* 
+* ('+', 4, 7) --> 11
+* ('-', 15, 18) --> -3
+* ('*', 5, 5) --> 25
+* ('/', 49, 7) --> 7 
+* 
+*/
+
 function basicOp(operation, value1, value2)
 {
     return typeof(operation);
